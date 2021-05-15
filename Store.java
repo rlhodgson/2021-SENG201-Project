232fb03ec3b402d31319d0ed1d460d53dae975ab
Rachel Hodgson <rlh89@uclive.ac.nz>
2021-05-15 18:21:39 +1200

    Update Store.java

--- Store.java
+++ Store.java
@@ -1,6 +1,23 @@
-package seng201Project;
+import java.util.ArrayList;
 
 public class Store {
+	ArrayList<Item> itemsSold = new ArrayList<Item>();
+	ArrayList<Item> itemsPurchased = new ArrayList<Item>();
 	
-}
+	Store(ArrayList<Item> itemsSoldin, ArrayList<Item> itemsPurchasedin) {
+		itemsSold = itemsSoldin;
+		itemsPurchased = itemsPurchasedin;
+	}
+	
+	void viewItems() {
+		System.out.println("Items the store sells: \n");
+		for (Item item : itemsSold) {
+			System.out.println(item.name + " $" + item.price + "\n");
+		}
+		System.out.println("Items the store buys: \n");
+		for (Item item : itemsPurchased) {
+			System.out.println(item.name + " $" + item.price + "\n");
+		}
+	}
 
+}
