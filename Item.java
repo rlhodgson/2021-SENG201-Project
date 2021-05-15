8bc690ef53aabb6405ebbfc2f9f1852eb6a6aeb3
Rachel Hodgson <rlh89@uclive.ac.nz>
2021-05-15 18:19:58 +1200

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,20 +1,27 @@
-package seng201Project;
 
 public class Item {
 	
 	String name;
 	int size;
 	String description;
-	int amountPaid;
-	int amountSold = 0;
-	String storeSold = null;
+	int price;
+	
+	int amountSold;
+	Island storeSold = null;
+	
+	Item(String namein, int pricein, int sizein, String descript) {
+		name = namein;
+		size = sizein;
+		price = pricein;
+		description = descript;
+		
+	}
 	
 	void getDescr() {
-		if (storeSold == null) {
-			System.out.println("Stuff that hasnt been sold");
-
+		if (storeSold != null) {
+			System.out.println(name + " sold for " + amountSold + " at the store on " + storeSold.name);
 		} else {
-			System.out.println("Stuff that has been sold");
+			System.out.println(name + " cost you " + price + " and is taking up " + size + " amount of cargo.");
 		}
 	}
 
