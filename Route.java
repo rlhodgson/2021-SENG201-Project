18387865cf1b474337273472db2ca9b0d1511d3a
Rachel Hodgson <rlh89@uclive.ac.nz>
2021-05-15 18:20:35 +1200

    Update Route.java

--- Route.java
+++ Route.java
@@ -1,19 +1,19 @@
-package seng201Project;
-
 public class Route {
 	String destIsland;
 	String origIsland;
 	int tripDist;
+	String routeDets;
 	
-	Route(String origIslandIn , String destIslandIn, int tripIn)	{
+	Route(String origIslandIn , String destIslandIn, int tripIn, String routeDetsin)	{
 		destIsland = destIslandIn;
 		origIsland = origIslandIn;
 		tripDist = tripIn;
+		routeDets = routeDetsin;
 	}
 	
 	void viewProperties() {
 		System.out.println("The distance from " + origIsland + " to " + destIsland + " is "
-				+ tripDist +" km.");
+				+ tripDist +" km. " + routeDets);
 	}
 	
 
