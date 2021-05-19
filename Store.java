4498498499bfb750649cdd4ac9fe34e0329ef8f7
Aidan Campbell <aca170@uclive.ac.nz>
2021-05-20 00:36:53 +1200

    Update Store.java

--- Store.java
+++ Store.java
@@ -1,23 +1,42 @@
 import java.util.ArrayList;
 
 public class Store {
-	ArrayList<Item> itemsSold = new ArrayList<Item>();
-	ArrayList<Item> itemsPurchased = new ArrayList<Item>();
+	private ArrayList<Item> itemsSold = new ArrayList<Item>();
+	private ArrayList<Item> itemsPurchased = new ArrayList<Item>();
 	
 	Store(ArrayList<Item> itemsSoldin, ArrayList<Item> itemsPurchasedin) {
-		itemsSold = itemsSoldin;
-		itemsPurchased = itemsPurchasedin;
+		this.itemsSold = itemsSoldin;
+		this.itemsPurchased = itemsPurchasedin;
 	}
 	
-	void viewItems() {
+	public void viewItems() {
 		System.out.println("Items the store sells: \n");
-		for (Item item : itemsSold) {
-			System.out.println(item.name + " $" + item.price + "\n");
-		}
+		viewSelling();
 		System.out.println("Items the store buys: \n");
-		for (Item item : itemsPurchased) {
-			System.out.println(item.name + " $" + item.price + "\n");
+		viewBuying();
+	}
+	
+	public void viewSelling() {
+		int i = 1;
+		for (Item item : this.itemsSold) {
+			System.out.println(i + ") " + item.getName() + " $" + item.getPrice() + " (" + item.getSize() + " space)\n");
+			i += 1;
 		}
 	}
+	
+	public void viewBuying() {
+		int i = 1;
+		for (Item item : this.itemsPurchased) {
+			System.out.println(i + ") " + item.getName() + " $" + item.getPrice() + "\n");
+			i += 1;
+		}
+	}
+	
+	public Item getItemSold(int index) {
+		return this.itemsSold.get(index);
+	}
 
+	public Item getItemPurchased(int index) {
+		return this.itemsPurchased.get(index);
+	}
 }
