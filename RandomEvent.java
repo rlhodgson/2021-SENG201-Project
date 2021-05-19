7c372ab69d9de9357cf4f60d3573bcb41d97e1e6
Aidan Campbell <aca170@uclive.ac.nz>
2021-05-20 00:37:15 +1200

    Update RandomEvent.java

--- RandomEvent.java
+++ RandomEvent.java
@@ -1,84 +1,85 @@
 import java.util.Random;
 
 public class RandomEvent {
-	int event;
-	Ship ship;
-	boolean pirates = false;
-	int totalGoodsAmount = 0;
-	int money = 0;
+	private Ship ship;
+	private boolean walkedPlank = false;
+	private int money = 0;
 	
-	RandomEvent(int eventInt, Ship myship) {
-		event = eventInt;
-		ship = myship;
-		if (event == 0) {
+	RandomEvent(int eventNum, Ship myShip) {
+		this.ship = myShip;
+		if (eventNum == 0) {
 			pirateEvent();
 		} 
-		else if (event == 1) {
+		else if (eventNum == 1) {
 			badWeather();
 		} 
-		else if (event == 2) {
+		else if (eventNum == 2) {
 			rescueSailors();
 		}
 	}
 		
-	void pirateEvent() {
+	public void pirateEvent() {
 		/* Random event of pirates occurring */
 		boolean upgrades = false;
-		for (Item i : ship.cargo) {
-			totalGoodsAmount += i.price;
-			if (i.name == "Upgraded Cannon" || i.name == "Upgraded Guns") {
+		int totalGoodsAmount = 0;
+		for (Item item : this.ship.getCargo()) {
+			totalGoodsAmount += item.getPrice();
+			if (item.getName() == "Upgraded Cannon" || item.getName() == "Upgraded Guns") {
 				upgrades = true;
 			}
 		}
 		Random rand = new Random();
-		int int_random = rand.nextInt(5);
-		if (upgrades == true && int_random <= 1) {
-			System.out.println("\nYou have been found by pirates, and they have taken your goods\n");
-			if (totalGoodsAmount >= 150) {
-				for (Item i : ship.cargo) {
-					ship.cargo.remove(i);
-					}
-				ship.cargoSpace = 0;
-				System.out.println("\nYour goods cost at least $150 which has satisfied the pirates\n");
-			} else {
-				System.out.println("\nYour goods have not satisfied the pirates. You must walk the plank.\n");
-				pirates = true;
-			}
-			/* they get caught*/
-		} 
-		else if (upgrades == false && int_random <= 2) {
-			System.out.println("\nYou have been found by pirates, and they have taken your goods\n");
-			if (totalGoodsAmount >= 150) {
-				for (Item i : ship.cargo) {
-					ship.cargo.remove(i);
-					}
-				ship.cargoSpace = 0;
-				System.out.println("\nYour goods cost at least $150 which has satisfied the pirates\n");
-			} else {
-				System.out.println("\nYour goods have not satisfied the pirates. You must walk the plank.\n");
-				pirates = true;
-			}
-			/* they get caught */
+		int int_random = rand.nextInt(6);
+		if (upgrades == true) {
+			int_random -= 1;
 		}
-	}
-	
-	void badWeather() {
-		/* Random event of bad weather occurring */
-		System.out.println("\nYou have encountered bad weather that has caused damage to your ship\n");
-		Random rand = new Random();
-		int int_random = rand.nextInt(9);
 		
-		ship.damage = int_random + 1;
-		ship.damageCost = ship.damage * 10;
-		System.out.println("\nThis will cost $"+ship.damageCost + " to repair. Repairs must be made before sailing again.\n");
+		if (int_random <= 1) {
+			System.out.println("\nYou have been found by pirates. Your crew was no match for them... You've walked the plank!\n");
+			this.walkedPlank = true;
+			
+		}else if (totalGoodsAmount >= 500) {
+			for (Item item : this.ship.getCargo()) {
+				this.ship.removeItem(item);
+			}
+			System.out.println("\nYou've been found by pirates! Your goods cost were worth at least $150 which has satisfied the pirates to leave you alone.\n");
+		} else {
+			System.out.println("\nYou've been found by pirates! Your goods weren't worth enough to satisfy the pirates. You must walk the plank!\n");
+			this.walkedPlank = true;
+		}
+			/* they get caught*/
+	} 
+
+	
+	
+	public void badWeather() {
+		/* Random event of bad weather occurring */
+		System.out.println("\nYou have encountered bad weather that has caused damage to your ship");
+		Random rand = new Random();
+		int int_random = rand.nextInt(5);
+		
+		int damage = int_random + 1 * 10;
+		this.ship.takeDamage(damage);
+		System.out.println("This will cost $"+ this.ship.getDamageCost() + " to repair. Repairs must be made before sailing again.\n");
 
 	}
 	
-	void rescueSailors() {
+	public void rescueSailors() {
 		/* Random event of sailors being rescued */
 		Random rand = new Random();
 		int int_random = rand.nextInt(5);
-		System.out.println("\nYou have rescued " + (int_random + 1) + " sailors who will give you $30 each as a reward\n");
-		money = (int_random + 1) * 30;
+		System.out.println("\nYou have rescued " + (int_random + 1) + " sailors who will gave you $30 each as a reward\n");
+		this.money = (int_random + 1) * 30;
 	}
+	
+	public int getMoney() {
+		return this.money;
+	}
+	
+	public boolean didWalkPlank() {
+		return this.walkedPlank;
+	}
+	
+	
+	
 }
