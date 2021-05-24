2c1569de15729fd5df2a5921e3642281d45b2667
Aidan Campbell <aca170@uclive.ac.nz>
2021-05-25 05:10:39 +1200

    Update Route.java

--- Route.java
+++ Route.java
@@ -1,22 +1,29 @@
 public class Route {
-	String destIsland;
-	String origIsland;
-	int tripDist;
-	String routeDets;
+	private String destIsland;
+	private String origIsland;
+	private int tripDist;
+	private int tripDays;
+	private String routeDets;
 	
-	Route(String origIslandIn , String destIslandIn, int tripIn, String routeDetsin)	{
-		destIsland = destIslandIn;
-		origIsland = origIslandIn;
-		tripDist = tripIn;
-		routeDets = routeDetsin;
+	Route(String origIslandIn , String destIslandIn, int tripIn, int shipSpeed, String routeDetsin)	{
+		this.destIsland = destIslandIn;
+		this.origIsland = origIslandIn;
+		this.tripDist = tripIn;
+		this.routeDets = routeDetsin;
+		this.tripDays = (int)(tripIn / shipSpeed / 24);
 	}
 	
-	public String viewProperties() {
-		String ans = "";
-		ans += ("The distance from " + origIsland + " to " + destIsland + " is "
-				+ tripDist +" km. " + routeDets + "\n");
-		return ans;
+	public String viewProperties(int wage) {
+		return "The distance from " + this.origIsland + " to " + this.destIsland + " is " + tripDist +" km. It will take " + 
+				this.tripDays + " days to get there. It will cost $" + (wage * this.tripDays) + ". "+ this.routeDets + "\n";
 	}
 	
+	public int getDays() {
+		return this.tripDays;
+	}
+	
+	public String getDestination() {
+		return this.destIsland;
+	}
 
 }
