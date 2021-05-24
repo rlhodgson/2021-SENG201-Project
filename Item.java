211751aa489b89ec2cbd4de307ddb5ee8dce0d8b
Aidan Campbell <aca170@uclive.ac.nz>
2021-05-25 05:11:36 +1200

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,35 +1,57 @@
 
 public class Item {
 	
-	String name;
-	int size;
-	String description;
-	int price;
+	private String name;
+	private int size;
+	private String description;
+	private int price;
 	
-	int amountSold;
-	Island storeSold = null;
+	// only used when the item is sold
+	private int amountSold;
+	private Island storeSold = null;
 	
 	Item(String namein, int pricein, int sizein, String descript) {
-		name = namein;
-		size = sizein;
-		price = pricein;
-		description = descript;
+		this.name = namein;
+		this.size = sizein;
+		this.price = pricein;
+		this.description = descript;
 		
 	}
 	
 	@Override
 	public String toString() {
-		return (name + " costs $" + price + " and takes up " + size + " amount of cargo.\n");
+		return (name + " costs $" + price + " and takes up " + size + " space.\n");
 	}
 	
 	public String getDescr() {
-		String ans = "";
+		String toReturn = "";
 		if (storeSold != null) {
-			ans += (name + " sold for " + amountSold + " at the store on " + storeSold.name + "\n");
+			toReturn += (name + " sold for " + amountSold + " at the store on " + storeSold.getName() + "\n");
 		} else {
-			ans += (name + " cost you " + price + " and is taking up " + size + " amount of cargo.\n");
+			toReturn += (name + " cost you " + price + " and is taking up " + size + " amount of cargo.\n");
 		}
-		return ans;
+		return	toReturn;
+	}
+	
+	public String getRawDescription() {		// used to return the raw string of description instead of the one above
+		return this.description;
+	}
+	
+	public int getSize() {
+		return this.size;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void changeSoldInfo(int soldFor, Island soldAtIsland) {
+		this.amountSold = soldFor;
+		this.storeSold = soldAtIsland;
 	}
 
 }
