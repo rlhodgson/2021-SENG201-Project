52d341d665bd64fc13327ea1e03ab47065fa3f26
Aidan Campbell <aca170@uclive.ac.nz>
2021-05-25 05:11:11 +1200

    Update Island.java

--- Island.java
+++ Island.java
@@ -2,50 +2,68 @@
 import java.util.ArrayList;
 
 public class Island {
-	String name;
-	ArrayList<Route> routes;
-	Store store;
-	
-	@Override
-	public String toString() {
-		return (name);
-	}
+	private String name;
+	private ArrayList<Route> routes;
+	private Store store;
 	
 	Island(String nname)	{
 		name = nname;
 		routes = new ArrayList<Route>();
 	}
 	
-	public String viewProperties() {
-		String ans = "";
-		
-		ans += (name + " has routes:\n");
-		for (Route i : routes) {
-			ans += i.viewProperties();
+	public String viewProperties(int wage) {
+		String toReturn = this.name + " has routes:\n";
+		for (Route route : this.routes) {
+			toReturn += route.viewProperties(wage);
 		}
 		
-		return ans;
-		/*need to do store still*/
+		return toReturn;
 	}
 	
-	public String visitStore() {
-		String ans = "";
-		ans += (name + " Store: \n");
-		ans += store.viewItems();
-		return ans;
+	public void getRoutes(int wage) {
+		int i = 1;
+		for (Route route: this.routes) {
+			System.out.print(i + ") ");
+			route.viewProperties(wage);
+			i ++;
+		}
 	}
 	
-	String getName() {
-		return  name;
+	public String getStoreItems() {
+		return this.store.viewItems();
 	}
 	
-	void addRoute(Route route) {
-		routes.add(route);
+	public String getName() {
+		return this.name;
 	}
 	
-	void addStore(Store storein) {
-		store = storein;
+	public void addRoute(Route route) {
+		this.routes.add(route);
 	}
- 
-}
+	
+	public void addStore(Store storein) {
+		this.store = storein;
+	}
+	
+	public ArrayList<Item> getStoreSelling() {
+		return this.store.getSelling();
+	}
+	
+	public ArrayList<Item> getStoreBuying() {
+		return this.store.getBuying();
+	}
+	
+	public Item getItemSold(int index) {
+		return this.store.getItemSold(index);
+	}
+	
+	public Item getItemPurchased(int index) {
+		return this.store.getItemPurchased(index);
+	}
+	
+	public Route getRoute(int index) {
+		return this.routes.get(index);
+	}
+	
+}	
 
