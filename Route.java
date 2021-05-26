6a63863e21ea6447b4c2ac0c7c1862da3bd11029
Rachel Hodgson <rlh89@uclive.ac.nz>
2021-05-26 15:06:55 +1200

    Update Route.java

--- Route.java
+++ Route.java
@@ -1,3 +1,8 @@
+/**Route class controlling route aspects
+ * 
+ * @author Rachel Hodgson and Aidan Campbell
+ *
+ */
 public class Route {
 	private String destIsland;
 	private String origIsland;
@@ -13,15 +18,28 @@ public class Route {
 		this.tripDays = (int)(tripIn / shipSpeed / 24);
 	}
 	
+	/**Returns a string of the properties of the route 
+	 * 
+	 * @param wage Cost per day to travel
+	 * @return String
+	 */
 	public String viewProperties(int wage) {
 		return "The distance from " + this.origIsland + " to " + this.destIsland + " is " + tripDist +" km. It will take " + 
 				this.tripDays + " days to get there. It will cost $" + (wage * this.tripDays) + ". "+ this.routeDets + "\n";
 	}
 	
+	/**Returns the number days the route will take
+	 * 
+	 * @return integer
+	 */
 	public int getDays() {
 		return this.tripDays;
 	}
 	
+	/**Returns the destination of the route
+	 * 
+	 * @return String
+	 */
 	public String getDestination() {
 		return this.destIsland;
 	}
